e8b04822090b605f95d4752076114b9bce5973de
Juan Pablo Rodriguez <pablojuan18sc35@gmail.com>
2025-05-13 02:01:12 -0400

    access to internet for api in main android manifest xml

--- lib/config/constants/app_api_constants.dart
+++ lib/config/constants/app_api_constants.dart
@@ -1,5 +1,6 @@
 class ApiConstants {
-  static const String baseUrl = "http://192.168.100.111:8000";
+  //static const String baseUrl = "http://192.168.100.111:8000";
+  static const String baseUrl = "http://157.230.230.131";
 
   static const String listProductsEndpoint = "/api/product";
   static const String listCategoriesEndpoint = "/api/categories";
